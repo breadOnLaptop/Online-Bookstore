27a0e443ad5ba99ea63c98e000a597931b30cb5e
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-12-23 11:10:20 +0530

    feat: created `Customer` entity

---
+++ src/main/java/com/peeyush/bookstore/entity/Customer.java
@@ -0,0 +1,35 @@
+package com.peeyush.bookstore.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    private List<Order> orders;
+}
